db626b8b8434e6e00e1d840337db5d6d10126955
nureazambd <nur.azam.bd@gmail.com>
2025-02-16 19:10:56 +0600

    modules in nodejs

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
+const math = require("./math.js")
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/add', (req, res)=>{
+    const sum =math.add(4, 6)
+    res.send(JSON.stringify(sum))
+    console.log(sum)
+})
+
+app.get('/sub', (req, res)=>{
+    const sub =math.sub(40, 6)
+    res.send(JSON.stringify(sub))
+    console.log(sub)
 })
 
 app.listen(3000)
