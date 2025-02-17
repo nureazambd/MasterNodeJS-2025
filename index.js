0dd5758d576137c9a04770975af26f178ed86a01
nureazambd <nur.azam.bd@gmail.com>
2025-02-17 22:11:12 +0600

    user created, edit and deleted api created

--- index.js
+++ index.js
@@ -17,10 +17,25 @@ app.get("/api/users", (req, res)=>{
     return res.json(users)
 })
 
+app.post("/api/users", (req, res)=>{
+    // create new user
+    return res.json({status: "pending"});
+})
+
 app.get("/api/users/:id", (req, res)=>{
-   const id = Number(req.params.id);
-   const user = users.find(user => user.id === id)
-   return res.json(user);
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id)
+    return res.json(user);
+ })
+
+app.patch("/api/users/:id", (req, res)=>{
+    // edit the user by id
+    return res.json({status: "pending"});
+})
+
+app.delete("/api/users/:id", (req, res)=>{
+    // deleted the user by id
+    return res.json({status: "pending"});
 })
 
 app.listen(port,()=>console.log(`server run on port: ${port}`))
